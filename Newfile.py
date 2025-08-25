e8f63a60e8b855175b4d3d7a25e6719f268c0f67
Harshitect <saoharshit05@gmail.com>
2025-08-26 04:55:47 +0530

    Function 3 Created

--- Newfile.py
+++ Newfile.py
@@ -1,7 +1,11 @@
 def left_triangle(n):
     for i in range(1, n + 1):
         print("* " * i)
-        
+
 def right_triangle(n):
     for i in range(1, n + 1):
         print("  " * (n - i) + "* " * i)
+
+def pyramid(n):
+    for i in range(1, n + 1):
+        print(" " * (n - i) + "* " * (2 * i - 1))
